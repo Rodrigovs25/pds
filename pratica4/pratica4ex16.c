a05c0d904a9b30e57530d1e959d4d57a823977f5
Rodrigovs25 <rodrigo2002.rvs@gmail.com>
2025-05-13 11:02:57 -0300

    pratica4

--- pratica4/pratica4ex16.c
+++ pratica4/pratica4ex16.c
@@ -8,12 +8,12 @@ int main(int argc, char const *argv[])
 	do
 	{
 		printf("Digite dois numeros:\n");
-		scanf("%d %d", &x, &f);
+		scanf("%d %d", &x, &y);
 		if(x <=0 || y <= 0)
 			printf("Valor invalido\n");
 	} while (x <=0 || y <= 0);
-	printf("\nO mdc de %d e %d eh %d", a, b, mdc(a, b));
-	printf("\nO mmc de %d e %d eh %d", a, b, mmc(a, b));
+	printf("\nO mdc de %d e %d eh %d", x, y, mdc(x, y));
+	printf("\nO mmc de %d e %d eh %d", x, y, mmc(x, y));
 	
 	return 0;
 }
